5c5befc942f24510b2c5c3e99cd339911efc8982
Manideep-Sitaram <manideepsitaram2024phase1@gmail.com>
2024-02-18 19:37:39 +0530

    Tailwind and props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 mb-4 rounded-xl'>Rukhmini is My Girl</h1>
+    <Card name = "Rukhmini"/>
+    <Card name = "Anu" />
+    </>
+  )
+}
+
+export default App

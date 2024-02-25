4453de6126f0331b8c39e3d08e0e8feebaac2b7b
Manideep-Sitaram <manideepsitaram2024phase1@gmail.com>
2024-02-25 22:03:41 +0530

    Functionality of the Currency Convertor Done

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -1,21 +1,70 @@
-import { useState } from 'react'
-import './App.css'
-import './index.css'
-import InputBox from './components/InputBox'
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import InputBox from "./components/InputBox";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
 
+  const data = useCurrencyInfo(from);
+
+  const currencyOptions = Object.keys(data);
+
+  function swap(e) {
+    e.preventDefault();
+    setFrom(to);
+    setTo(from);
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
+  }
+
+  function convert(){
+    setConvertedAmount(amount * data[to])
+  }
+  
   return (
     <>
-     <div className="p-8 bg-slate-300 max-w-md mx-auto">
-      <InputBox label = "From"/>
-      <button className='py-2 text-white bg-blue-500 rounded m-1 px-3'>swap</button>
-      <InputBox label = "To"/>
-      <button className='w-full bg-blue-600 rounded-xl px-2 py-3 my-4 text-white'>Convert USD to INR</button>
-     </div>
+      <form onSubmit={ e => {
+        e.preventDefault();
+        convert()
+      }}>
+        <div className="w-full h-screen flex flex-wrap justify-center items-center">
+          <div className="p-8 bg-slate-300 max-w-md">
+            <InputBox
+              label="From"
+              amount={amount}
+              selectCurrency={from}
+              currencyOptions={currencyOptions}
+              onAmountChange={amount => setAmount(amount)}
+              onCurrencyChange={currency => setFrom(currency)}
+            />
+            <button
+              className="py-2 text-white bg-blue-500 rounded m-1 px-3"
+              onClick={swap}
+            >
+              swap
+            </button>
+            <InputBox
+              label="To"
+              amount={convertedAmount}
+              selectCurrency={to}
+              currencyOptions={currencyOptions}
+              onAmountChange={amount => setAmount(amount)}
+              onCurrencyChange={currency => setTo(currency)}
+              amountInputDisable={true}
+            />
+            <button className="w-full bg-blue-600 rounded-xl px-2 py-3 my-4 text-white" >
+              Convert {from.toUpperCase()} to {to.toUpperCase()}
+            </button>
+          </div>
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 06currencyConvertor/src/components/InputBox.jsx
+++ 06currencyConvertor/src/components/InputBox.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
+import React, { useId } from "react";
 function InputBox({
-  label
+  label,
+  amount,
+  selectCurrency,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  amountInputDisable = false,
 }) {
+  const uniqueId = useId();
   return (
-    <div className='px-2 py-7 bg-white rounded-xl flex flex-wrap justify-between'>
-        <div className='flex flex-col flex-wrap items-baseline gap-3'>
-        <label htmlFor={label}>{label}</label>
-        <input type="number" name="" id={label} placeholder='0'/>
-        </div>
+    <div className="px-2 py-7 bg-white rounded-xl flex flex-wrap justify-between">
+      <div className="flex flex-col flex-wrap items-baseline gap-3">
+        <label htmlFor={uniqueId}>{label}</label>
+        <input
+          type="number"
+          id={uniqueId}
+          disabled={amountInputDisable}
+          placeholder="Amount"
+          value={String(amount)}
+          onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+        />
+      </div>
 
-        <div className='flex flex-col flex-wrap items-baseline gap-3'> 
-            <label htmlFor="">Currency Type</label>
-            <select name="" id="">
-                <option value="">usd</option>
-            </select>
-        </div>
+      <div className="flex flex-col flex-wrap items-baseline gap-3">
+        <label htmlFor="">Currency Type</label>
+        <select
+          name=""
+          id=""
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          value={selectCurrency}
+        >
+          {currencyOptions.map((item) => (
+            <option value={item} key={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
-  )
+  );
 }
 
-export default InputBox
+export default InputBox;
